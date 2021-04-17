e8c155c44c82ed441cf3f5ad944da7c738b67442
DHFellow082218 <jtmcsjester@yahoo.com>
2021-04-17 22:13:09 +0800

    :ambulance: Removed Unused Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,9 +19,6 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('new-component', require('./components/NewComponent.vue').default);
-Vue.component('post-form-component', require('./components/PostFormComponent.vue').default);
 Vue.component('post-app-component', require('./components/Post/PostApp.vue').default);
 
 /**
