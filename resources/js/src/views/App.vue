4743b0ff42c17cde46067723beaaecc40db1ad6d
DHFellow082218 <jtmcsjester@yahoo.com>
2021-07-26 11:53:32 +0800

    :coffin: Removed unused router transition code

--- resources/js/src/views/App.vue
+++ resources/js/src/views/App.vue
@@ -44,20 +44,4 @@
   {
     opacity: 0;
   }
-
-  @keyframes fadeIn
-  {
-    0%
-    {
-      opacity : 0;
-    }
-    50%
-    {
-      opacity : 0.5;
-    }
-    100%
-    {
-      opacity : 1;
-    }
-  }
 </style>
